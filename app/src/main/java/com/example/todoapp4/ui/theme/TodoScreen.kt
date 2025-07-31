98415edbc6b41b190a7f61fc5aa570f1c0174e18
beyzza <beyzkrc.0@gmail.com>
2025-07-31 03:38:22 +0300

    Todo ekranı tasarlandı ve ViewModel ile bağlandı.

--- app/src/main/java/com/example/todoapp4/ui/theme/TodoScreen.kt
+++ app/src/main/java/com/example/todoapp4/ui/theme/TodoScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 
+
 @Composable
 fun TodoScreen(viewModel: TodoViewModel) {
     val todoList = viewModel.todoList.collectAsState().value
