d35ad79dfd495acf2846e1e94a937ce7056a7cf7
beyzza <beyzkrc.0@gmail.com>
2025-07-31 03:37:01 +0300

    MVVM yapısında ViewModel sınıfı eklendi.

--- app/src/main/java/com/example/todoapp4/ui/theme/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapp4/ui/theme/TodoViewModel.kt
@@ -1,28 +1,34 @@
 package com.example.todoapp4
 
+
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 data class TodoItem(val title: String, val isDone: Boolean = false)
 
+
 class TodoViewModel : ViewModel() {
+
     private val _todoList = MutableStateFlow<List<TodoItem>>(emptyList())
     val todoList: StateFlow<List<TodoItem>> get() = _todoList
 
     fun addTask(title: String) {
+
         val currentList = _todoList.value.toMutableList()
         currentList.add(TodoItem(title = title))
         _todoList.value = currentList
     }
 
     fun toggleTask(task: TodoItem) {
+
         _todoList.value = _todoList.value.map {
             if (it == task) it.copy(isDone = !it.isDone) else it
         }
     }
 
     fun removeTask(item: TodoItem) {
+
         _todoList.value = _todoList.value - item
     }
 }
